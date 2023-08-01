305f4b593c848a43cbebf2267606a0b18a9da019
Oğuz Kaan Yazan <89526970+oguzyazan-infoline@users.noreply.github.com>
2023-08-01 18:44:09 +0300

    Identity Server added

---
+++ src/KontrolBulutu.Server.IdentityServer/ServerBrandingProvider.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.Ui.Branding;
+using Volo.Abp.DependencyInjection;
+
+namespace KontrolBulutu.Server
+{
+    [Dependency(ReplaceServices = true)]
+    public class TieredBrandingProvider : DefaultBrandingProvider
+    {
+        public override string AppName => "Tiered";
+    }
+}
